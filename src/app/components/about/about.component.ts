4963f845ff3fbe311e32b1067829b511579106e9
muhd-kifayath <imkifayath@gmail.com>
2023-11-19 20:47:08 +0530

    Project Component Responsiveness

--- src/app/components/about/about.component.ts
+++ src/app/components/about/about.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./about.component.scss']
 })
 export class AboutComponent {
-
+  aboutme = "I'm a software engineering student at VIT Chennai with experience in web development (React.js and Express.js) and Android app development. I also love designing user-friendly web and mobile applications using Figma. I'm excited to keep learning and transform ideas into real solutions.";
 }
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
-
+  name = 'Kifayath';
+  quality = '[passionate: programmer, creative: designer]';
 }
